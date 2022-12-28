0ba739a59f713e9187911f078e4a81b11d7d4e49
Florent VICENTE <vicentegroupe@gmail.com>
2022-12-28 10:56:00 +0100

    accordion project ok

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [ info, setInfo ] = useState(data)
+  return (
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {info.map(item => <SingleQuestion key={item.id} {...item} />)}
+        </section>
+      </div>
+    </main>
+  )
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ id, title, info }) => {
+  const [ isClosed, setIsClosed ] = useState(true)
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={() => setIsClosed(prevState => !isClosed)}>
+          {isClosed ? <AiOutlinePlus /> : <AiOutlineMinus />}
+        </button>
+      </header>
+      {!isClosed && <p>{info}</p>}
+    </article>
+  )
 };
 
 export default Question;
