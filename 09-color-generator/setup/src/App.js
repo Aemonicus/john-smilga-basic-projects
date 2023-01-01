8444d4159d6c291a2a7d829f04029c7e18238c61
Florent VICENTE <vicentegroupe@gmail.com>
2023-01-01 16:56:17 +0100

    color generator project ok

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SingleColor from './SingleColor'
 
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [ startingColor, setCurrentColor ] = useState("rgb( 241, 80, 37 )")
+  const [ newColor, setNewColor ] = useState()
+  const [ allColors, setAllColors ] = useState([])
+  const [ colorError, setColorError ] = useState(false)
+
+
+  const handleChange = e => {
+    setColorError(false)
+    setNewColor(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    try {
+      createPalette(newColor)
+    } catch (error) {
+      setColorError(true)
+      alert("Code couleur au mauvais format")
+      console.log(error)
+    }
+  }
+
+  const createPalette = (color) => {
+    const itemColor = new Values(color)
+
+    setAllColors(itemColor.all(10))
+  }
+
+
+  useEffect(() => {
+    createPalette(startingColor)
+  }, [])
+
+  return <>
+    <section className="container">
+      <h3>color generator</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='#f15025 / rgb(255,255,255) format' value={newColor} className={colorError ? "error" : ""} onChange={handleChange} />
+        <button className="btn">submit</button>
+      </form>
+    </section>
+    <section className="colors">
+      {allColors.map(item => <SingleColor key={Date.now().toString(36) + Math.random().toString(36)} {...item} rgbString={item.rgbString()} hexString={item.hexString()} />)}
+    </section>
+  </>
 }
 
 export default App
--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,28 @@
 import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({ weight, type, rgbString, hexString }) => {
+  const [ copiedColor, setCopiedColor ] = useState("")
+
+  const copyText = () => navigator.clipboard.writeText(copiedColor)
+
+  const handleClick = e => {
+    const color = e.target.children[ 1 ].innerText
+    setCopiedColor(color)
+
+    alert(`Color ${color} copied!`)
+  }
+
+  useEffect(() => {
+    copyText()
+  }, [ copiedColor ])
+
+  const styles = type === "shade" ? "color color-light" : "color false"
+
+  return (
+    <article className={styles} style={{ backgroundColor: rgbString }} onClick={handleClick}>
+      <p className='percent-value'>{weight} %</p>
+      <p className="color-value">{hexString}</p>
+    </article>
+  )
 }
-
 export default SingleColor
