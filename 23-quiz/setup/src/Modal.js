e3b293841d6a04428173e429d3998355dc49c8e8
Florent VICENTE <vicentegroupe@gmail.com>
2023-01-20 15:32:55 +0100

    quiz project ok

--- 23-quiz/setup/src/Modal.js
+++ 23-quiz/setup/src/Modal.js
@@ -2,7 +2,17 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Modal = () => {
-  return <h2>modal component</h2>
+  const { isModalOpen, closeModal, correct, questions } = useGlobalContext()
+
+  return <div className={`${isModalOpen ? "modal-container isOpen" : "modal-container"}`}>
+    <div className="modal-content">
+      <h2>Congrats!</h2>
+      <p>You answered {correct} of {questions.length} questions correctly</p>
+      <button className="close-btn" onClick={closeModal}>
+        Play again
+      </button>
+    </div>
+  </div>
 }
 
 export default Modal
