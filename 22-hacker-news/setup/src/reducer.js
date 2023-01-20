af46bca7e179a38c885c47c720a5fda535606ae7
Florent VICENTE <vicentegroupe@gmail.com>
2023-01-20 11:04:30 +0100

    hacker news project ok

--- 22-hacker-news/setup/src/reducer.js
+++ 22-hacker-news/setup/src/reducer.js
@@ -6,5 +6,41 @@ import {
   HANDLE_SEARCH,
 } from './actions'
 
-const reducer = () => {}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return { ...state, isLoading: true }
+
+    case SET_STORIES:
+      return { ...state, isLoading: false, hits: action.payload.hits, nbPages: action.payload.nbPages }
+
+    case REMOVE_STORY:
+      return { ...state, hits: state.hits.filter(story => story.objectID !== action.payload) }
+
+    case HANDLE_SEARCH:
+      return { ...state, query: action.payload, page: 0 }
+
+    case HANDLE_PAGE:
+      let value
+
+      if (action.payload === "increase") {
+        value = state.page + 1
+        if (value > state.nbPages - 1) {
+          value = 0
+        }
+      }
+
+      if (action.payload === "decrease") {
+        value = state.page - 1
+        if (value < 0) {
+          value = state.nbPages - 1
+        }
+      }
+
+      return { ...state, page: value }
+
+    default:
+      throw new Error(`no matching "${action.type}" action type`)
+  }
+}
 export default reducer
